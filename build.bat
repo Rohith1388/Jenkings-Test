919600f6bb3d1feca9f792d63182a6b309a6388d
Rohith1388 <runrajarun31@gmail.com>
2025-06-26 16:39:05 +0530

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,3 +1,8 @@
-bat
-CopyEdit
-echo Hello from Jenkins CI
+@echo off
+REM Create an HTML file
+(
+echo ^<html^>^<body^>^<h1^>Hello from Jenkins^</h1^>^</body^>^</html^>
+) > output.html
+
+REM Create a zip file
+powershell Compress-Archive -Path output.html -DestinationPath output.zip
